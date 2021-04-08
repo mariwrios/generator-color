cf6534564d864e8bbf5bbb533106b1ff308e4d89
Maria Jaramillo <milagrosmjr20@gmail.com>
2021-04-08 02:09:09 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./index.css"
 
-function App() {
+const App = () => {
+  const [cardColor, setCardColor] = useState("")
+
+  const randomColor = () => {
+    const hexaColor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+    let cajita = ""
+
+    for (let i = 1; i <= 6; i++) {
+      let azar = 0 + Math.random() * (15 - 0)
+      cajita = cajita + hexaColor[parseInt(azar)]
+    }
+    return cajita
+  }
+
+  const handleColor = () => {
+    setCardColor(() => randomColor().length >= 6 && randomColor())
+  }
+
+  const color = {
+    height: "23vh",
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    width: 250,
+    background: `#${cardColor}`
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container-card ">
+        <div style={color}></div>
+        <button onClick={handleColor}>click me</button>
+        <p>#{cardColor}</p>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
